5bf36dc42dedb57e613deace0e8d418a3e0473a2
linh luong <vietlinh.it55@gmail.com>
2013-09-30 00:47:25 +0700

    Tuần 2 fix

--- triangle/test.py
+++ triangle/test.py
@@ -1,6 +1,6 @@
 ﻿import unittest
 from math import sqrt
-from triangle import checkTriangle
+from check import checkTriangle
 class MyTest(unittest.TestCase):
     def test_1_1(self):
         self.assertEqual(checkTriangle(-1,2,4),-1)
@@ -22,12 +22,13 @@ class MyTest(unittest.TestCase):
         self.assertEqual(checkTriangle(4,1,2),0)
     def test_3(self): 
         self.assertEqual(checkTriangle(1,2,4),0)
-	def test_4(self): 
-        self.assertEqual(checkTriangle(1,1,1),0)
-    def test_5(self): 
+    def test_4(self): 
         self.assertEqual(checkTriangle(1,4,2),0)
-    def test_6(self): 
+    def test_5(self): 
         self.assertEqual(checkTriangle(2,2,2),1)
+    def test_6(self): 
+        self.assertEqual(checkTriangle((sqrt(2)),(sqrt(2)),2),2)
+        #test này em chưa hiểu tại sao lại trả về 4
     def test_7(self): 
         self.assertEqual(checkTriangle(2,2,3),4)
     def test_8(self): 
